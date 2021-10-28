2e68968ce78d421571cde227cc8fcf6b6f083a3d
huzaifa hassan <huzaifahassan97@hotmail.com>
2021-10-28 11:45:29 +0500

    Model

---
+++ JqueryAjax/Models/TransactionModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JqueryAjax.Models
+{
+    public class TransactionModel
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        [Column(TypeName = "nvarchar(12)")]
+        [DisplayName("Account Number")]
+        [Required(ErrorMessage = "This Field is Required")]
+        [MaxLength(12, ErrorMessage = "Maximum 12 characters only")]
+        public string AccountNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Benificiary Name")]
+        [Required(ErrorMessage = "This Field is Required")]
+        public string BeneficiaryName { get; set; }
+
+        [Column(TypeName = "nvarchar(11)")]
+        [DisplayName("SWIFT CODE")]
+        [Required(ErrorMessage = "This Field is Required")]
+        [MaxLength(11)]
+        public string SWIFTCODE { get; set; }
+
+        [DisplayName("Amount")]
+        [Required(ErrorMessage = "This Field is Required")]
+        public int Amount { get; set; }
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime Date { get; set; }
+
+    }
+}
